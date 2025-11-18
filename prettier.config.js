4f2b338b5164d01f33a31ed789bbfb86c2ce52f8
Mike MacCana <mike.maccana@gmail.com>
2025-11-18 13:48:30 -0500

    Basic project

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+// Normally I hate modifying prettier defaults as they're usually nitpicky, but Solana Kit tends to use longer names, 
+// so we need to increase the print width to avoid single function calls spilling over into multiple lines
+
+export default {
+  printWidth: 120,
+};
